5c780f8786283f7a37df0db12e26264204273b6a
jtsapos <jtsapos@gmx.com>
2022-01-31 16:06:42 -0500

    Updated scripts.js using jquery

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function newItem() {
+
+  //1. Explanation: Adding a new item to the list of items:
+  //let li = document.createElement('li');
+  //let inputValue = document.getElementById('input').value;
+  //let text = document.createTextNode(inputValue);
+  //li.appendChild(text);
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must enter some information');
+  } else {
+    //let list = document.querySelector('#list');
+    //list.appendChild(li);
+    $('#list').append(li);
+  }
